223f7c0e4d9f5110e4d5fa9d7683d6a5f2d902bf
okinejeremiah9-ship-it <okinejeremiah9@gmail.com>
2025-11-12 21:23:37 +0000

    Update index.ts

--- supabase/functions/auth-signup/index.ts
+++ supabase/functions/auth-signup/index.ts
@@ -52,8 +52,8 @@ Deno.serve(async (req: Request) => {
 
   try {
     const supabaseAdmin = createClient(
-Deno.env.get("PROJECT_URL")
-Deno.env.get("SERVICE_ROLE_KEY")
+  Deno.env.get("SUPABASE_URL")!,
+  Deno.env.get("SUPABASE_SERVICE_ROLE_KEY")!
 
     );
 
